ea0444d59d70930a79fae341e5fa977dc5d03007
rebay1982 <reda.baydoun@gmail.com>
2017-11-01 16:39:26 -0400

    More stuff (shellcode)

--- 2_3_syscall/asm_syscall.asm
+++ 2_3_syscall/asm_syscall.asm
@@ -7,36 +7,18 @@ global asm_syscall
 
 section .text
 asm_syscall:
-    ; Please write your code here
 		push ebp
 		mov ebp, esp
 
 		mov eax, [ebp + 0x08]
+		mov ebx, [ebp + 0x0C]
+		mov ecx, [ebp + 0x10]
+		mov edx, [ebp + 0x14]
+		mov esi, [ebp + 0x18]
+		mov edi, [ebp + 0x1C]
+		mov ebp, [ebp + 0x20]
 
-		cmp eax, __NR_fork
-		je asm_fork
-
-		cmp eax, __NR_write
-		je asm_write
-
-		cmp eax, __NR_exit
-		je asm_exit
-
-asm_execute_syscall:
 		int 0x80
 
 		pop ebp
 		ret
-
-asm_fork:
-		jmp asm_execute_syscall
-
-asm_write:
-		mov ebx, [ebp + 0x0C]
-		mov ecx, [ebp + 0x10]
-		mov edx, [ebp + 0x14]
-		jmp asm_execute_syscall
-
-asm_exit:
-		mov ebx, [ebp + 0x0C]
-		jmp asm_execute_syscall
